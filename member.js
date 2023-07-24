0657a3085a210bdc7bf8af78f0be010f402ab5bd
Aman Kumar Gupta <87111503+Aman-Kr-Gupta@users.noreply.github.com>
2023-07-24 05:17:10 +0000

    Copilot second  commit]

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember(){
+    document.getElementById("skills").style.display = "block";
+    document.getElementById("member").style.display = "none";
+    document.getElementById("project").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
